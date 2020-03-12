f2ed1a8cdc22613759a59ea75cf62e2d4680790f
Yuhei Kikuta <conan0226.yuu@gmail.com>
2020-03-12 12:15:23 +0900

    generate controller

---
+++ app/helpers/future_taxes_helper.rb
@@ -0,0 +1,2 @@
+module FutureTaxesHelper
+end
---
+++ test/controllers/future_taxes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FutureTaxesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
