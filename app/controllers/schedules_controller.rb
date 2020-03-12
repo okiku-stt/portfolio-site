8b06491d8f7dbaeaa7bbb1881164cf10808bd5df
Yuhei Kikuta <conan0226.yuu@gmail.com>
2020-03-12 12:19:03 +0900

    add controller action

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,30 @@
 class SchedulesController < ApplicationController
+
+  def index
+    @schedules = Schedule.where(company_id: params[:company_id]).order(:year).order(:month)
+
+  end
+
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    @schedule = PastComTax.new(tax_params)
+    if @schedule.save
+      redirect_to company_schedules_path(@company)
+    else
+      flash.now[:notice] = "エラーが発生しました。もう一度入力してください。"
+      render :new
+    end
+  end
+
+  private
+
+  def set_company
+    @company = Company.find(params[:company_id])
+  end
+  def tax_params
+    params.require(:schedule).permit(:year,:month,:company_tax,:consumption_tax).merge(company_id: @company.id)
+  end
 end
